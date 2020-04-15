08511da25e448f7873c703d3265fc2583d018323
ekgorshkova <63596262+ekgorshkova@users.noreply.github.com>
2020-04-15 15:34:21 +0300

    Инсталяция Джанго

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Applications/PyCharm.app/Contents/bin/https:/github.com/ekgorshkova/Gorshkova/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

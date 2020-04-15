b9d341f7663f63a0b69612febe47d70b0c7ba1b0
ekgorshkova <63596262+ekgorshkova@users.noreply.github.com>
2020-04-15 18:36:16 +0300

    Первое занятие

--- Gorshkova/settings.py
+++ Gorshkova/settings.py
@@ -30,16 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = {
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'main'
+}
 
-MIDDLEWARE = [
+MIDDLEWARE = {
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+}
 
 ROOT_URLCONF = 'Gorshkova.urls'
 
